c89df1bfd613765a1abb63735ef44d0206ffcc1d
Stone Werner <stonewerner@berkeley.edu>
2024-08-27 11:31:10 -0700

    fixed clerk middleware for protecting routes 54:22

--- intellidrive/app/page.tsx
+++ intellidrive/app/page.tsx
@@ -7,12 +7,38 @@ import {
   UserButton
 } from '@clerk/nextjs'
 import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Link from 'next/link'
+
 
 export default function Home() {
   return (
     <main className="">
-      <h1>IntelliDrive</h1>
-      <Button variant={"secondary"}>My Demo Button</Button>
+      <div>
+        <div className="p-10 flex flex-col bg-[#2B2929]
+              dark:bg-slate-800 text-white space-y-5">
+          <h1 className="text-5xl font-bold">
+            Welcome to IntelliDrive. <br />
+            <br />
+            Storing everything for you and your academic needs. All in one place, supercharged by AI.
+          </h1>
+          <p className="pb-20">
+            Enhance your personal storage with IntelliDrive, a fast, efficient, and AI-powered solution
+            to upload, organize, search, and access files from anywhere.
+            Securely store important documents and media, and experience the convenience of easy file management and sharing
+            in one centralized AI-solution.
+          </p>
+          <Link href="/dashboard" className="flex cursor-pointer bg-blue-500 p-5 w-fit">
+            Try it for free!
+            <ArrowRight className="ml-10" />
+          </Link>
+        </div>
+      </div>
+      
+
+
+
+
     </main>
   );
 }
--- intellidrive/middleware.ts
+++ intellidrive/middleware.ts
@@ -1,4 +1,4 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+/*import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware()
 // By default all routes are public
@@ -11,3 +11,23 @@ export const config = {
     '/(api|trpc)(.*)',
   ],
 };
+*/
+//////////////////////////////////////////////////////////////////////////////
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect()
+  }
+})
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
