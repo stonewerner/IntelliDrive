8c03e6082151285fcfb03eb89e3ea0ad97e5c156
Stone Werner <stonewerner@berkeley.edu>
2024-08-28 07:12:51 -0700

    added delete modal 2:27:30

--- intellidrive/components/table/Table.tsx
+++ intellidrive/components/table/Table.tsx
@@ -15,6 +15,11 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import { PencilIcon, TrashIcon } from "lucide-react"
+import { Button } from "../ui/button"
+import { FileType } from "@/typings";
+import { useAppStore } from "@/store/store"
+import { DeleteModal } from "../DeleteModal"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -29,7 +34,26 @@ export function DataTable<TData, TValue>({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
-  })
+  });
+
+  const [setIsDeleteModalOpen, setFileId, setFilename, setIsRenameModalOpen] =
+    useAppStore((state) => [
+      state.setIsDeleteModalOpen,
+      state.setFileId,
+      state.setFilename,
+      state.setIsRenameModalOpen,
+    ]);
+
+  const openDeleteModal = (fileId: string) => {
+    setFileId(fileId);
+    setIsDeleteModalOpen(true);
+  };
+
+  const openRenameModal = (fileId: string, filename: string) => {
+    setFileId(fileId);
+    setFilename(filename);
+    setIsRenameModalOpen(true);
+  }
 
   return (
     <div className="rounded-md border">
@@ -59,11 +83,48 @@ export function DataTable<TData, TValue>({
                 key={row.id}
                 data-state={row.getIsSelected() && "selected"}
               >
+                <DeleteModal />
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    {cell.column.id == 'timestamp' ? (
+                      <div className="flex flex-col">
+                        <div className="text-sm">
+                          {(cell.getValue() as Date).toLocaleDateString()}
+                        </div>
+                        <div className="text-xs text-gray-500">
+                          {(cell.getValue() as Date).toLocaleDateString()}
+                        </div>
+                      </div>
+                    ) : cell.column.id === 'filename' ? (
+                      <p
+                        onClick={() => {
+                          openRenameModal(
+                            (row.original as FileType).id,
+                            (row.original as FileType).filename
+                          )
+                        }}
+                        className="underline flex items-center text-blue-500 hover:cursor-pointer"
+                        >
+                          {cell.getValue() as string}{" "}
+                          <PencilIcon size={15} className="ml-2" />
+                      </p>
+                    ) : (
+                      flexRender(cell.column.columnDef.cell, cell.getContext())
+                    )}
                   </TableCell>
                 ))}
+                
+                <TableCell key ={(row.original as FileType).id}>
+                  <Button
+                    variant={"outline"}
+                    onClick={() => {
+                      openDeleteModal((row.original as FileType).id)
+                    }}
+                    >
+                    <TrashIcon size={20} />
+                  </Button>
+
+                </TableCell>
               </TableRow>
             ))
           ) : (
